54c27314ded16e067d7831bc360244891c8b4361
Yao-DJAKA <157647680+Yao-DJAKA@users.noreply.github.com>
2024-07-11 14:12:13 -0400

    Update basicMap.js

--- basicMap.js
+++ basicMap.js
@@ -1,10 +1,10 @@
 console.log("JS File was loaded");
 
-// 35.217665 N, 80.831473 W -> 35.217665, -80.831473
+// 35.2176665 N, 80.831473 W -> 35.2176665, -80.831473
 
 let map = L.map("map", {
-  center: [35.217665, -80.831473],
-  zoom: 8
+  center: [35.2176665, -80.831473],
+  zoom: 15
 });
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
