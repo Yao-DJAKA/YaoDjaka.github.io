1e023567bd1e898ba65933d321c4e28be6106b9e
Yao-DJAKA <157647680+Yao-DJAKA@users.noreply.github.com>
2024-07-18 14:22:14 -0400

    Update earthquakes.js

--- earthquakes.js
+++ earthquakes.js
@@ -2,7 +2,14 @@ let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geo
 
 d3.json(url).then(
   function(data)
-  {
-    console.log(data);
-  }
+    {
+     
+      console.log(data);
+  
+      let map = L.map("map",{
+        center: [37.09, -95.71],
+        zoom: 5 
+      });
+      
+    }
 );
